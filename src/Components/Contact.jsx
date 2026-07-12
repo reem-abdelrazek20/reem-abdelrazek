7fb73c1e55126a6677b647f0c96327008169de92
Reem Abdelrazek <reemsadek2000@gmail.com>
2026-07-12 23:05:31 +0300

    Update contact section design

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,163 +1,74 @@
-import { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
-import "../styles/contactStyle.css"
+import "../styles/contactStyle.css";
 
 export default function Contact() {
-  const formRef = useRef();
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_he3d75c", //  Service ID
-        "template_slahkwh", //  Template ID
-        formRef.current,
-        "owkq6S9XvntM4iELN" //  Public Key
-      )
-      .then(
-        (result) => {
-          console.log(" تم الإرسال:", result.text);
-
-          setFormData({ name: "", email: "", message: "" });
-        },
-        (error) => {
-          console.log(" حصل خطأ:", error.text);
-          alert(" حدث خطأ أثناء الإرسال");
-        }
-      );
-  };
-
   return (
-    <section id="contact">
+    <section id="contact ">
       <h2 className="section-header">Contact</h2>
-
-      <div className="contact-wrapper">
-        <form
-          ref={formRef}
-          id="contact-form"
-          className="form-horizontal"
-          role="form"
-          onSubmit={handleSubmit}
-        >
-          <div className="form-group">
-            <div className="col-sm-12">
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                placeholder="NAME"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
+      <p className="section-subheader">
+        Let's connect! I'm always open to discussing new opportunities and
+        exciting projects.
+      </p>
+      <div className="cards-contact">
+        <div className="card-contact">
+          <h2 className="card-h2 ">Let's Work Together</h2>
+          <p>
+            I'm a Front-End Developer passionate about building responsive,
+            user-friendly, and modern web applications.
+          </p>
+          <div className="buttons ">
+            <button>Send Email</button>
+            <button>Download CV</button>
           </div>
-
-          <div className="form-group">
-            <div className="col-sm-12">
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="EMAIL"
-                name="email" // ← نفس اسم القالب
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </div>
-
-          <textarea
-            className="form-control"
-            rows="10"
-            placeholder="MESSAGE"
-            name="message" // ← نفس اسم المتغير في قالب EmailJS
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-
-          <button
-            className="btn btn-primary send-button"
-            id="submit"
-            type="submit"
-          >
-            <div className="alt-send-button">
-              <i className="fa fa-paper-plane"></i>
-              <span className="send-text">SEND</span>
-            </div>
-          </button>
-        </form>
-
-        <div className="direct-contact-container">
-          <ul className="contact-list">
-            <li className="list-item">
-              <i className="fa-solid fa-location-dot"></i>
-              <span className="contact-text place">Esna - Luxor, Egypt</span>
-            </li>
-            <li className="list-item">
-              <i className="fa-solid fa-phone"></i>
-              <span className="contact-text phone">+201141272473</span>
-            </li>
-            <li className="list-item">
-              <i className="fa fa-envelope fa-2x" style={{color:"#E0DFFF"}}> </i>
-              <span className="contact-text gmail">
-                <a href="mailto:reemsadek2000@gmail.com">
-                  reemsadek2000@gmail.com
-                </a>
-              </span>
-            </li>
-          </ul>
-
-          <hr />
-          <ul className="social-media-list">
+        </div>
+        <div className="card-contact">
+          <h2 className="card-h2">Contact Information</h2>
+          <ul>
             <li>
-              <a
-                href="https://github.com/reem-abdelrazek20"
-                target="_blank"
-                className="contact-icon"
-              >
-                <i className="fa-brands fa-github"  style={{ color: "#9290C3" }}></i>
-              </a>
+              <strong>
+                <i className="fa-solid fa-location-dot"></i> Location:
+              </strong>
+              Esna - Luxor, Egypt
             </li>
             <li>
-              <a href="#" target="_blank" className="contact-icon">
-                <i className="fa-brands fa-codepen" style={{ color: "#535C91" }}></i>
-              </a>
+              <strong>
+                {" "}
+                <i className="fa-solid fa-phone"></i>Phone:
+              </strong>{" "}
+              <a href="tel:+201141272473">+20 11 4127 2473</a>
             </li>
             <li>
-              <a href="#" target="_blank" className="contact-icon">
-                <i className="fa-brands fa-twitter" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" target="_blank" className="contact-icon">
-                <i className="fa-brands fa-instagram" aria-hidden="true"></i>
+              <strong>
+                <i className="fa-solid fa-envelope"></i> Email:
+              </strong>{" "}
+              <a href="mailto:reemsadek2000@gmail.com">
+                reemsadek2000@gmail.com
               </a>
             </li>
           </ul>
+          <div className="social-icons mt-8">
+            <h2>Find me on</h2>
 
-          <hr />
-          <div className="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
+            <a
+              href="https://www.linkedin.com/in/رابط-حسابك"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+
+            <a
+              href="https://github.com/reem-abdelrazek20"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
         </div>
       </div>
+
+      <hr />
+      <div className="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
     </section>
   );
 }
